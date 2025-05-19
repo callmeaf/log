fe521f5eb8ce1aa7f318dba878bd432be9595d7a
callmeaf <alikhoshkar2000@gmail.com>
2025-05-19 10:18:38 +0330

    add HasSearch

--- src/App/Models/Log.php
+++ src/App/Models/Log.php
@@ -4,10 +4,11 @@ namespace Callmeaf\Log\App\Models;
 
 use Callmeaf\Base\App\Models\BaseLogModel;
 use Callmeaf\Base\App\Traits\Model\HasDate;
+use Callmeaf\Base\App\Traits\Model\HasSearch;
 
 class Log extends BaseLogModel
 {
-    use HasDate;
+    use HasDate,HasSearch;
     protected $table = 'activity_log';
 
     public static function configKey(): string
@@ -19,6 +20,24 @@ class Log extends BaseLogModel
     {
         return [
             ...(self::config()['enums'] ?? []),
+        ];
+    }
+
+    public function searchParams(): array
+    {
+        return [
+            [
+                'subject_id' => 'subject_id',
+                'subject_type' => 'subject_type',
+                'causer_id' => 'causer_id',
+                'causer_type' => 'causer_type',
+                'event' => 'event',
+                'log_name' => 'log_name',
+            ],
+            [
+                'created_from' => 'created_at',
+                'created_to' => 'created_at',
+            ]
         ];
     }
 }
--- src/App/Services/LogService.php
+++ src/App/Services/LogService.php
@@ -19,7 +19,7 @@ class LogService
          * @var ActivityLogger $activity
          */
         $activity = $closure($activity);
-        
+
         return $activity->withProperty('job_type',$jobClass)->log("{$jobClass} has been executed.");
     }
 }
